bbb804350b7ef9e3cb7a9a27dded6391a86a56fb
Subhan Khan <subhank1997@gmail.com>
2023-06-09 16:00:33 -0400

    added an image and have the faqs to the right of the image. Also added a component that shows the answer to the question you clicked on. (clicking on a question opens up all the answers. Have to rewrite the toggleAnswer component.

--- faq-accordion-card/src/App.js
+++ faq-accordion-card/src/App.js
@@ -1,18 +1,51 @@
 import './App.css';
+import React, { useState } from 'react';
+import image from './images/illustration-woman-online-desktop.svg'
 
 function App() {
+  const [showAnswers, setShowAnswers] = useState(false);
+
+  const toggleAnswers = () => {
+    setShowAnswers(!showAnswers);
+  };
+
   return (
     <div class="main">
       <div class="card">
-        <div class="title">
-          <h1>FAQ</h1>
+
+        <div class="image-container">
+          <img class='woman-computer' src={image} alt="" />
         </div>
 
-        <div class="list">
-          <h4 class="questions">How many team memebers can i invite</h4>
-          <p>1</p>
-        </div>
+        <div class='content-container'>
+          <div class="title">
+            <h1>FAQ</h1>
+          </div>
 
+          <div class="list">
+            <div>
+              <h4 class="questions" onClick={toggleAnswers}>How many team memebers can i invite</h4>
+              {showAnswers && <p className="answers">1</p>}
+            </div>
+            <div>
+              <h4 class="questions" onClick={toggleAnswers}>What is the maximum file upload size</h4>
+              {showAnswers && <p className="answers">1</p>}
+            </div>
+            <div>
+              <h4 class="questions" onClick={toggleAnswers}>How do i rest my password</h4>
+              {showAnswers && <p className="answers">1</p>}
+            </div>
+            <div>
+              <h4 class="questions" onClick={toggleAnswers}>Can I cancel my subscription</h4>
+              {showAnswers && <p className="answers">1</p>}
+            </div>
+            <div>
+              <h4 class="questions" onClick={toggleAnswers}>Do you provide additonal support</h4>
+              {showAnswers && <p className="answers">1</p>}
+            </div>
+          </div>
+
+        </div>
       </div>
     </div>
   );
