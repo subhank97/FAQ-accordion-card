022a1ee308577c188e162ead280eb9d980f19995
Subhan Khan <subhank1997@gmail.com>
2023-06-09 15:29:18 -0400

    created card component and updated background

---
+++ faq-accordion-card/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div class="main">
+      <div class="card">
+        <div class="title">
+          <h1>FAQ</h1>
+        </div>
+
+        <div class="list">
+          <h4 class="questions">How many team memebers can i invite</h4>
+          <p>1</p>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
