4c658a6e5eb95ca111320c6715b88cca17a0bc75
Subhan Khan <subhank1997@gmail.com>
2023-07-27 18:39:19 -0400

    updated questions to be an array to be mapped over. Also updated toggleAnswers to use the index to find which question is toggled

--- faq-accordion-card/src/App.js
+++ faq-accordion-card/src/App.js
@@ -3,11 +3,32 @@ import React, { useState } from 'react';
 import image from './images/illustration-woman-online-desktop.svg'
 
 function App() {
-  const [showAnswers, setShowAnswers] = useState(false);
+  const [showAnswers, setShowAnswers] = useState({});
 
-  const toggleAnswers = () => {
-    setShowAnswers(!showAnswers);
+  const toggleAnswers = (index) => {
+    setShowAnswers((prevState) => ({
+      ...prevState,
+      [index]: !prevState[index],
+    }));
   };
+
+  const freQuestion = [
+    {
+      "How many team memebers can i invite": "1"
+    },
+    {
+      "What is the maximum file upload size": "1"
+    },
+    {
+      "How do i rest my password": "1"
+    },
+    {
+      "Can I cancel my subscription": "1"
+    },
+    {
+      "Do you provide additonal support": "1"
+    }
+  ]
 
   return (
     <div class="main">
@@ -18,31 +39,22 @@ function App() {
         </div>
 
         <div class='content-container'>
+
           <div class="title">
             <h1>FAQ</h1>
           </div>
 
-          <div class="list">
-            <div>
-              <h4 class="questions" onClick={toggleAnswers}>How many team memebers can i invite</h4>
-              {showAnswers && <p className="answers">1</p>}
-            </div>
-            <div>
-              <h4 class="questions" onClick={toggleAnswers}>What is the maximum file upload size</h4>
-              {showAnswers && <p className="answers">1</p>}
-            </div>
-            <div>
-              <h4 class="questions" onClick={toggleAnswers}>How do i rest my password</h4>
-              {showAnswers && <p className="answers">1</p>}
-            </div>
-            <div>
-              <h4 class="questions" onClick={toggleAnswers}>Can I cancel my subscription</h4>
-              {showAnswers && <p className="answers">1</p>}
-            </div>
-            <div>
-              <h4 class="questions" onClick={toggleAnswers}>Do you provide additonal support</h4>
-              {showAnswers && <p className="answers">1</p>}
-            </div>
+          <div className="list">
+            {freQuestion.map((question, index) => (
+              <div key={index}>
+                <h4 className="questions" onClick={() => toggleAnswers(index)}>
+                  {Object.keys(question)[0]}
+                </h4>
+                {showAnswers[index] && (
+                  <p className="answers">{Object.values(question)[0]}</p>
+                )}
+              </div>
+            ))}
           </div>
 
         </div>
